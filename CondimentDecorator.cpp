e968d8103c5989793524427e03919335e9bf17b7
Daniil Yavorsckiy <64340796+DaniilYavorskiy@users.noreply.github.com>
2022-05-03 15:23:28 +0700

    Delete CondimentDecorator.cpp

--- CondimentDecorator.cpp
+++
@@ -1,9 +0,0 @@
-#include "CondimentDecorator.h"
-
-CondimentDecorator::CondimentDecorator()
-    description = "Dobavka";
-}
-
-char* CondimentDecorator::getDescription() {
-      return description;
-}
